17df9388c61f8298ecec2524b3b463064a8104e9
luiz <luizhenriquetrovao9@gmail.com>
2022-07-06 21:00:40 -0300

    added-gsap-tl-and-color-tween

--- script.js
+++ script.js
@@ -3,14 +3,24 @@
 async function updateBarHeight() {
   const response = await fetch('./data.json');
   const json = await response.json();
+  let biggestBarValue = 0;
+  let biggestBar;
   for (let i = 0; i < barsEl.length; i++) {
     barsEl[i].style.height = `${json[i].amount * 2.5}px`;
+    if (json[i].amount > biggestBarValue) {
+      biggestBarValue = json[i].amount;
+      biggestBar = barsEl[i];
+    }
   }
-
-  gsap.from('.bar', {
+  biggestBar.classList.add('biggestBar');
+  const tl = gsap.timeline();
+  tl.from('.bar', {
     height: 0,
-    duration: 1.5,
+    duration: 1.25,
     ease: 'bounce.out',
+  }).to('.biggestBar', {
+    backgroundColor: 'hsl(186, 34%, 60%)',
+    duration: 0.5,
   });
 }
 
