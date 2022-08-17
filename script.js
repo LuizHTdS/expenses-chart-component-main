96b3a476dc421725cca116d23e5f98927143a5c1
luiz <luizhenriquetrovao9@gmail.com>
2022-08-17 13:13:50 -0300

    fixed animation

--- script.js
+++ script.js
@@ -5,25 +5,31 @@ async function updateBarHeight() {
   const json = await response.json();
   let biggestBarValue = 0;
   let biggestBar;
+  let curBar;
   for (let i = 0; i < barsEl.length; i++) {
+    curBar = barsEl[i];
+    curBar.classList.add('cur_bar');
     barsEl[i].style.height = `${json[i].amount * 2.5}px`;
+    tl.from('.cur_bar', {
+      height: 0,
+      duration: 0.35,
+      ease: 'bounce.out',
+    });
+    curBar.classList.remove('cur_bar');
+
     if (json[i].amount > biggestBarValue) {
       biggestBarValue = json[i].amount;
       biggestBar = barsEl[i];
     }
   }
   biggestBar.classList.add('biggestBar');
-  const tl = gsap.timeline();
-  tl.from('.bar', {
-    height: 0,
-    duration: 1.25,
-    ease: 'bounce.out',
-  }).to('.biggestBar', {
+  tl.to('.biggestBar', {
     backgroundColor: 'hsl(186, 34%, 60%)',
     duration: 0.5,
   });
 }
 
+const tl = gsap.timeline();
 const barsEl = document.querySelectorAll('.bar');
 
 updateBarHeight();
